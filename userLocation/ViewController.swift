90f7f3d9a81582fe3e7b38a27e0f55de59ece4ee
Paige Plander <paigeplan@berkeley.edu>
2017-05-01 12:32:07 -0700

    Added a mapview and a pin

--- userLocation/ViewController.swift
+++ userLocation/ViewController.swift
@@ -8,41 +8,56 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
 class ViewController: UIViewController,CLLocationManagerDelegate {
     
+    @IBOutlet weak var myMapView: MKMapView!
+    
     let manager = CLLocationManager()
     
-    @IBAction func button(_ sender: Any) {
-    }
-    
+    // To keep track of where the user currently is. Default at Berkeley
+    var userCurrentLocation: CLLocation = CLLocation(latitude: 51, longitude: -0.1276250)
+
     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        print("Hello")
+        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let userLocation:CLLocation = locations[0]
-        let long = userLocation.coordinate.longitude;
-        let lat = userLocation.coordinate.latitude;
+        userCurrentLocation = locations[0]
+        let long = userCurrentLocation.coordinate.longitude;
+        let lat = userCurrentLocation.coordinate.latitude;
         
         print(long, lat)
         
-        //Do What ever you want with it
+        // Create a rectangle around the users location and display it in the map
+        let span = MKCoordinateSpanMake(0.1, 0.1)
+        let regionAroundUser = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userCurrentLocation.coordinate.latitude, longitude: userCurrentLocation.coordinate.longitude), span: span)
+        myMapView.setRegion(regionAroundUser, animated: true)
+        
+        // Set a marker for the user's current location
+        let currentLocationMarker = MKPointAnnotation()
+        currentLocationMarker.coordinate = userCurrentLocation.coordinate
+        currentLocationMarker.title = "Current Location"
+        myMapView.addAnnotation(currentLocationMarker)
+        
+        // TODO: Find locations nearby for the user. Here's a simple example on how to do this
+        // https://github.com/paigeplan/mapkit-query-demo/blob/master/Mapkit-Demo/MapViewController.swift
+        // (look at the part about MKLocalSearch)
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if CLLocationManager.locationServicesEnabled() {
             manager.requestAlwaysAuthorization()
-        manager.requestWhenInUseAuthorization()
+            manager.requestWhenInUseAuthorization()
             manager.delegate = self
             manager.desiredAccuracy = kCLLocationAccuracyBest
             manager.startUpdatingLocation()
         }
         let currRegion = CLCircularRegion(center: CLLocationCoordinate2D(latitude: 51, longitude: -0.1276250), radius: 1000, identifier: "Berkeley")
         manager.startMonitoring(for: currRegion)
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
